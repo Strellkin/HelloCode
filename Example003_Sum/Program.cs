2395eb85b64f0304f0d6e1ec6bfd305ac4617326
Sergey <sergey.leffchenko@gmail.com>
2023-04-20 19:34:59 +0200

    пытаюсь исправить ошибку сборки в Example004_Div

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -3,3 +3,4 @@ int numberB = 5;
 int result = numberA + numberB;
 Console.WriteLine(result);
 
+
--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,4 @@
-﻿int numberA = 10;
-int numberB = 5;
+﻿double numberA = 12;
+double numberB = 5;
 Console.Writeline(numberA / numberB);
+
